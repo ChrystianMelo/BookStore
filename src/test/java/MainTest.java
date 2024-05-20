dc53c261de05c0b968c8544a2c6c90bb5464d346
Sanny Moreira <78819896+Snycms@users.noreply.github.com>
2024-05-20 20:59:50 -0300

    Add base do projeto

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ */
+public class MainTest {
+
+    @Test
+    public void testCorrect() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void testWrong() {
+        assertEquals(1, 2);
+    }
+}
