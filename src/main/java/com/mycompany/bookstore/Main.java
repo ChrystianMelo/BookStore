d430bedbe5f25c510bdd89ebcf145621033b2b79
Chrystian Melo <chrysmelao@gmail.com>
2024-05-24 11:25:34 -0300

    Refactoring: Fixing some comments.

--- src/main/java/com/mycompany/bookstore/Main.java
+++ src/main/java/com/mycompany/bookstore/Main.java
@@ -1,6 +1,5 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Main.java
  */
 package com.mycompany.bookstore;
 
