8436936d8ce6af82b2c9f1e1123a6d67e75f0c6a
Chrystian Melo <chrysmelao@gmail.com>
2024-05-24 22:22:17 -0300

    Removed unknown test case.

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,12 +1,3 @@
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
-
 public class MainTest {
-
-    @Test
-    public void testCorrect() {
-        assertEquals(1, 1);
-    }
-
 }
