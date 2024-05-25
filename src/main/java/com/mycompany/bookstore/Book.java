09b697d5a62ab170732b9d99e4f921e1d0509f40
Chrystian Melo <chrysmelao@gmail.com>
2024-05-24 22:16:46 -0300

    Added more tests. Added comparing method to Book class.

--- src/main/java/com/mycompany/bookstore/Book.java
+++ src/main/java/com/mycompany/bookstore/Book.java
@@ -53,6 +53,24 @@ public class Book {
         this.value = value;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        return id == ((Book) obj).id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
     /**
      * Obtém o identificador do livro.
      *
--- src/test/java/BookStoreTest.java
+++ src/test/java/BookStoreTest.java
@@ -1,5 +1,11 @@
 
+import com.mycompany.bookstore.Book;
 import com.mycompany.bookstore.BookStore;
+import com.mycompany.bookstore.Customer;
+import com.mycompany.bookstore.Supplier;
+import java.util.ArrayList;
+import java.util.HashMap;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,7 +20,59 @@ public class BookStoreTest {
     }
 
     @Test
-    public void testSuppliersCount() {
+    public void testEmptyAvailableBooks() {
         assertTrue(bookStore.getAvailableBooks().isEmpty());
+    }
+
+    @Test
+    public void testAddCustomer() {
+        Customer customer = new Customer("test_customer", "Test Customer", null);
+        bookStore.addCostumer(customer);
+        assertTrue(bookStore.getCostumers().contains(customer));
+    }
+
+    @Test
+    public void testAddSupplier() {
+        Supplier supplier = new Supplier("test_supplier", "Test Supplier", null);
+        bookStore.addSupplier(supplier);
+        assertTrue(bookStore.getSuppliers().contains(supplier));
+    }
+
+    @Test
+    public void testSetCustomers() {
+        ArrayList<Customer> customers = new ArrayList<>();
+        customers.add(new Customer("customer1", "Customer 1", null));
+        customers.add(new Customer("customer2", "Customer 2", null));
+        bookStore.setCostumers(customers);
+        assertEquals(customers, bookStore.getCostumers());
+    }
+
+    @Test
+    public void testSetSuppliers() {
+        ArrayList<Supplier> suppliers = new ArrayList<>();
+        suppliers.add(new Supplier("supplier1", "Supplier 1", null));
+        suppliers.add(new Supplier("supplier2", "Supplier 2", null));
+        bookStore.setSuppliers(suppliers);
+        assertEquals(suppliers, bookStore.getSuppliers());
+    }
+
+    @Test
+    public void testGetAvailableBooks() {
+        Supplier supplier1 = new Supplier("supplier1", "Supplier 1", null);
+        Supplier supplier2 = new Supplier("supplier2", "Supplier 2", null);
+
+        supplier1.registerBook(new Book(1, "Book 1", 10.0f, "Details"), 5);
+        supplier1.registerBook(new Book(2, "Book 2", 15.0f, "Details"), 3);
+        supplier2.registerBook(new Book(3, "Book 3", 20.0f, "Details"), 7);
+
+        bookStore.addSupplier(supplier1);
+        bookStore.addSupplier(supplier2);
+
+        HashMap<Book, Integer> expectedAvailableBooks = new HashMap<>();
+        expectedAvailableBooks.put(new Book(1, "Book 1", 10.0f, "Details"), 5);
+        expectedAvailableBooks.put(new Book(2, "Book 2", 15.0f, "Details"), 3);
+        expectedAvailableBooks.put(new Book(3, "Book 3", 20.0f, "Details"), 7);
+
+        assertTrue(bookStore.getAvailableBooks().keySet().containsAll(expectedAvailableBooks.keySet()));
     }
 }
