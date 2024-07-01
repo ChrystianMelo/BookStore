cf5c4ce488e52f2a18dd24feaba447d41f4c2c84
Chrystian Melo <chrysmelao@gmail.com>
2024-07-01 19:04:42 -0300

    Fixed error in 8e09fec

--- src/main/java/com/mycompany/bookstore/Main.java
+++ src/main/java/com/mycompany/bookstore/Main.java
@@ -17,8 +17,6 @@ public class Main {
     public static void main(String[] args) {
         BookStore bookStore = new BookStore();
 
-        BookStoreDatabaseUtils db = new BookStoreDatabaseUtils();
-
         BookStoreGUI gui = new BookStoreGUI(bookStore);
         gui.setVisible(true);
     }
